9843ef20433e99d1ae346f4bc9b3c39c9a86996f
DrEvery <omeguswatchtower@gmail.com>
2025-05-02 01:24:03 -0600

    Added Datagen Added Cup of Joe

---
+++ src/main/java/com/hamburger/oof/datagen/LanguageGenerator.java
@@ -0,0 +1,21 @@
+package com.hamburger.oof.datagen;
+
+import com.hamburger.oof.Oof;
+import com.hamburger.oof.world.item.ModCreativeTabs;
+import com.hamburger.oof.world.item.ModItems;
+import com.hamburger.oof.world.level.block.ModBlocks;
+import net.minecraft.data.PackOutput;
+import net.neoforged.neoforge.common.data.LanguageProvider;
+
+public class LanguageGenerator extends LanguageProvider {
+    public LanguageGenerator(PackOutput output) {
+        super(output, Oof.MODID, "en_us");
+    }
+
+    @Override
+    protected void addTranslations() {
+        this.addItem(ModItems.CUP_OF_JOE, "SCP-198");
+
+        this.addBlock(ModBlocks.HUME_INFUSER, "Hume Infuser");
+    }
+}
